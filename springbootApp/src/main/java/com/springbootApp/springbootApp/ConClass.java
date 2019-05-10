486763e3873f30ff30b55df25cb8013262708bcf
Ramachandra Maddela <Ramachandra Maddela@Ramchandra>
2019-05-10 09:41:48 +0530

    First Commit into SpringbootApp

---
+++ springbootApp/src/main/java/com/springbootApp/springbootApp/ConClass.java
@@ -0,0 +1,15 @@
+package com.springbootApp.springbootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/App")
+public class ConClass {
+
+	@GetMapping("/getSay")
+	public String say(){
+		return"Hi";
+	}
+}
